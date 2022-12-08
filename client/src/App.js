1b5a3b3376c5e9d089c6d31fc14b80a3ca100d3c
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-12-08 15:15:59 -0700

    clients loading to page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
+import Header from "./component/Header";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import Clients from "./component/Clients"
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+})
 
 
 function App() {
   return (
+    <>
+    <ApolloProvider client={client}>
+    <Header />
     <div className="container">
-      <h1>Hello World!</h1>
+      <Clients />
     </div>
+    </ApolloProvider>
+    </>
   );
 }
 
