e6ea34f5e8c014e3dd535be3722066cdf1424759
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2023-01-03 09:29:01 -0700

    added photos and clients with active projects

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import vistaHome from "./assets/vistaHome.png";
+import vistaView from "./assets/vistaView.png";
 import vistaHome from "./assets/vistaHome.png";
 
 
@@ -8,8 +8,9 @@ const Header = () => {
     <nav className="navbar-nav navbar-light bg-primary mb-4 p-0" >
       <div className="container">
           <div className="d-flex justify-content-center align-items-center">
-            <img src={vistaHome} alt="familyPic" className="mr-2" width="100" height="100"/>
-            <h3>Mountain House Management App</h3>
+            <img src={vistaHome} alt="familyPic" className="img" width="100" height="100"/>
+            <h3>Mountain House Project App</h3>
+            <img src={vistaView} alt="familyPic" className="img" width="100" height="100"/>
           </div>
       </div>
     </nav>
